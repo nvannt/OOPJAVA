27028099556c20a71bfc390c42379fb5f371e0cb
nvannt <ngayaythatdep2512@gmail.com>
2024-01-31 09:23:23 +0700

    Slide92B1

---
+++ BTTH/PTB1.JAVA
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class PTB1 {
+    public static void main(String[] args){
+        double hesoA,hesoB,nghiemX;
+        Scanner sc = new scanner(System.in);
+        System.out.println(x:"A=");
+        hesoA = sc .nextDouble();
+        Scanner sc = new scanner(System.in);
+        System.out.println(x:"B=");
+        hesoB = sc .nextDouble();
+        }
+    if(hesoA == 0 ){
+        System.out.println(x:"Khong phai phuong trinh bac nhat")
+    }else{
+        nghiemX = -hesoB/hesoA;
+    }
+   }
